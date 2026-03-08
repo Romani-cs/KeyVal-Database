453eaad106d8c3a2cde6a2ddee5629a23818dfbe
Romani Whittaker <romaniwhittaker@my.unt.edu>
2026-03-08 03:49:22 -0500

    Added CLI Command parsing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<sstream>
 using namespace std;
 
 int main(){
+	string line;
 
+	while (getline(cin, line))
+	{
+		stringstream ss(line);
+		string command;
+
+		ss >> command;
+
+		if(command == "SET")
+		{
+			cout << "SET command received" << endl;
+		}
+		else if(command == "GET")
+		{
+			cout << "GET command received" << endl;
+		}
+		else if(command == "EXIT")
+		{
+			break;
+		}
+	}
 
 	return 0;
 }
