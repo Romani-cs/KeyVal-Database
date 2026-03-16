da31b875b8de105c2c4768809d606cd8635a38d1
Romani Whittaker <romaniwhittaker@my.unt.edu>
2026-03-15 20:41:26 -0500

    Key value storage

--- main.cpp
+++ main.cpp
@@ -5,6 +5,45 @@
 #include<sstream>
 using namespace std;
 
+struct Entry {
+	string key;
+	string value;
+};
+
+class KeyValueStore
+{
+	private:
+		vector<Entry> index;
+	public:
+		int findKey(const string &key) 
+		{
+			for(int i=0; i < index.size(); i++)
+			{
+				if(index[i].key == key){
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		void set(const string &key, const string &value)
+		{
+			int pos = findKey(key);
+
+			if(pos == -1)
+			{
+				Entry e;
+				e.key = key;
+				e.value = value;
+				index.push_back(e);
+			}
+			else
+			{
+				index[pos].value = value;
+			}
+		}
+};
+
 int main(){
 	string line;
 
